cacf55b0e89b64acf521f1b8c475610da06bf5ef
matheus mendes <matheusmendesreis@outlook.com>
2023-11-11 15:52:23 -0300

    feat:estrutura da pagina home pronta e etilizada

--- matheusmendes.com/src/app/(home)/page.tsx
+++ matheusmendes.com/src/app/(home)/page.tsx
@@ -1,8 +1,25 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home</h1>
-    </main>
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <nav className="w-[122.41px] h-[148px] ">
+        <ul className="flex gap-4 justify-center">
+          <li>
+            <Link href="/projetos" className="w-[77px] h-[17px] text-zinc-500 text-lg font-normal font-['Inter'] leading-tight">Projetos</Link>
+          </li>
+          <li>
+            <Link href="/contatos" className="w-[77px] h-[17px] text-zinc-500 text-lg font-normal font-['Inter'] leading-tight" >contatos</Link>
+          </li>
+        </ul>
+      </nav>
+      <main className="">
+        <p className=" w-[560.11px] h-[119px] text-[126.25px] font-normal font-['Inter'] leading-[94.72px] tracking-[4.85px] text-center	">matheus</p>
+      </main>
+      <footer>
+      <p className="w-full h-[17px] text-center text-zinc-500 text-lg font-normal font-['Inter'] leading-tight">Olá, eu sou o Matheus, eu construo sites</p>
+      </footer>
+    </div>
   )
 }
+
