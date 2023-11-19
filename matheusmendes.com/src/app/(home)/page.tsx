7500459f3ea67560c8044dba414b1fea2517447d
matheus mendes <matheusmendesreis@outlook.com>
2023-11-19 10:22:54 -0300

    feat:adicionnado responsividade

--- matheusmendes.com/src/app/(home)/page.tsx
+++ matheusmendes.com/src/app/(home)/page.tsx
@@ -10,15 +10,15 @@ export default function Home() {
       <nav className="w-[122.41px] h-[148px] ">
         <ul className="flex gap-4 justify-center">
           <li>
-            <Link href={data.links[0].href} className="w-[77px] h-[17px] text-zinc-500 text-lg font-normal font-['Inter'] leading-tight" title={`ir para ${data.links[0].text}`}>{data.links[0].text}</Link>
+            <Link href={data.links[0].href} className=" w-[4.8125rem] h-[1.0625rem] text-zinc-500 text-lg font-normal font-['Inter'] leading-tight" title={`ir para ${data.links[0].text}`}>{data.links[0].text}</Link>
           </li>
           <li>
-            <Link href={data.links[1].href} className="w-[77px] h-[17px] text-zinc-500 text-lg font-normal font-['Inter'] leading-tight" title={`ir para ${data.links[1].text}`}>{data.links[1].text}</Link>
+            <Link href={data.links[1].href} className=" w-[4.8125rem] h-[1.0625rem] text-zinc-500 text-lg font-normal font-['Inter'] leading-tight" title={`ir para ${data.links[1].text}`}>{data.links[1].text}</Link>
           </li>
         </ul>
       </nav>
       <main className="">
-        <p className=" w-[560.11px] h-[119px] text-[126.25px] font-normal font-['Inter'] leading-[94.72px] tracking-[4.85px] text-center	" title={`Olá, sou o Matheus`}>{data.mainText.content}</p>
+        <p className="w-full sm:w-[35rem] h-[7.4375rem] sm:text-9xl text-8xl font-normal font-['Inter'] leading-[94.72px] tracking-[4.85px] text-center	" title={`Olá, sou o Matheus`}>{data.mainText.content}</p>
       </main>
       <footer>
         <p className="w-full h-[17px] text-center text-zinc-500 text-lg font-normal font-['Inter'] leading-tight">{data.footerText.content}</p>
