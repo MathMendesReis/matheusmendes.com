f6e196ab83a80b6708a742bd82711ef143c051ed
Matheus Mendes <matheusmendesreis@outlook.com>
2023-11-29 10:30:02 -0300

    animation na home adicionanada

--- matheusmendes.com/tailwind.config.ts
+++ matheusmendes.com/tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from 'tailwindcss'
 
+
+
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,34 +10,58 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      animation: {
-        wiggle: 'wiggle 1s ease-in-out infinite',
-        title: 'wiggle 1s ease-in-out infinite',
-      },
       keyframes: {
         wiggle: {
-          '0%, 100%': { transform: 'rotate(-3deg)' },
-          '50%': { transform: 'rotate(3deg)' },
+          '0%': {
+            transform: 'scaleY(0)',
+            opacity: '0',
+
+          },
+          '100%': {
+            transform: 'scaleY(1)',
+            opacity: '100',
+
+          },
         },
         title: {
-          "0%": {
-            "line-height": "0%",
-            "letter-spacing": "0.25em",
-            opacity: "0",
-          },
-          "25%": {
-            "line-height": "0%",
-            opacity: "0%",
-          },
-          "80%": {
-            opacity: "100%",
-          },
+					"0%": {
+						"line-height": "0%",
+						"letter-spacing": "0.25em",
+						opacity: "0",
+					},
+					"25%": {
+						"line-height": "0%",
+						opacity: "0%",
+					},
+					"80%": {
+						opacity: "100%",
+					},
 
-          "100%": {
-            "line-height": "100%",
-            opacity: "100%",
-          },
-        },
+					"100%": {
+						"line-height": "100%",
+						opacity: "100%",
+					},
+          
+				},
+        "fade-in": {
+					"0%": {
+						opacity: "0%",
+					},
+					"75%": {
+						opacity: "0%",
+					},
+					"100%": {
+						opacity: "100%",
+					},
+				},
+      },
+      animation: {
+        wiggle: 'wiggle 2s ease 0s 1 normal forwards',
+        title: "title 3s ease-out forwards",
+        "fade-in": "fade-in 3s ease-in-out forwards",
+
+
+
       }
     },
   },
