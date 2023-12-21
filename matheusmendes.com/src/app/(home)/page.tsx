861b853d933326e3821d662f23a48fbaf79c7bf7
Matheus <matheusmendesreis@outlook.com>
2023-12-20 22:35:26 -0300

    styled:adicionando responsividade

--- matheusmendes.com/src/app/(home)/page.tsx
+++ matheusmendes.com/src/app/(home)/page.tsx
@@ -11,11 +11,11 @@ export default function Home() {
       <div className="flex flex-col justify-center items-center min-h-screen ">
         <div className="flex flex-col justify-center items-center ">
           <nav className="w-full sm:w-[20.625rem] h-[5.25rem]  pt-3 ">
-            <ul className="w-full flex gap-4 justify-center animate-fade-in  ">
+            <ul className="w-full flex gap-4 justify-center animate-fade-in px-3  ">
               {data.links.map((item, i) => {
                 if (item.href !== '/') {
                   return (
-                    <Link key={i} href={item.href} className=" min-w-[5.8125rem] min-h-[1.0625rem]"       title={`ir para ${item.text}`}>
+                    <Link key={i} href={item.href} className=" sm:min-w-[5.8125rem] min-h-[1.0625rem]"       title={`ir para ${item.text}`}>
                       <p className=" text-zinc-500 sm:text-lg font-normal font-['Inter'] leading-tight">{item.text}</p>
                     </Link>
                   )
@@ -24,30 +24,17 @@ export default function Home() {
             </ul>
           </nav>
           <main className="flex flex-col">
-            <h1 className="z-10 text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display text-8xl whitespace-nowrap bg-clip-text sm:text-9xl ">
+            <h1 className="z-10 text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display text-6xl whitespace-nowrap bg-clip-text sm:text-9xl ">
               {data.mainText.content}
             </h1>
              
           </main>
           <footer className="h-[5.25rem] flex sm:items-center justify-center animate-fade-in mt-8 flex-col">
-            <p className="w-full  h-[1.0625rem] text-center text-zinc-500 sm:text-lg font-normal font-['Inter'] leading-tight">{data.footerText.content}</p>
+            <p className="w-full  min-h-[1.0625rem] text-center text-zinc-500 sm:text-lg font-normal font-['Inter'] leading-tight">{data.footerText.content}</p>
            
           </footer>
         </div>
       </div>
-      {/* <nav className="animate-fade-in mt-10 absolute bottom-1/2">
-        <ul className=" flex items-center justify-center gap-3 py-2 px-4 flex-col">
-          {contatos.map(({icon,url})=>(
-            <>
-              <li >
-                <Link href={url} target="_blank" className="">
-                {icon}
-                </Link>
-              </li>
-            </>
-          ))}
-        </ul>  
-      </nav> */}
     </>
   )
 }
--- matheusmendes.com/src/app/(subpaginas)/layout.tsx
+++ matheusmendes.com/src/app/(subpaginas)/layout.tsx
@@ -20,21 +20,21 @@ export default function RootLayout({
   const [home, ...others] = data.links
   return (
     <div className='flex flex-col items-center animate-title'>
-      <header className=" z-30 h-[4.5rem] w-full sm:w-[1024px] fixed backdrop-filter backdrop-grayscale backdrop-blur-md backdrop-contrast-100 flex items-center justify-between sm:p-0 px-8">
+      <header className=" z-30 h-[4.5rem] w-full lg:w-[1000px] fixed backdrop-filter backdrop-grayscale backdrop-blur-md backdrop-contrast-100 flex items-center justify-between sm:p-0 px-8">
         <Link href={home.href} className=''>
           <ArrowLeft />
         </Link>
         <section className=" h-6  sm:flex gap-5 hidden">
           {others.map(({href,text},index) => {
             return (
-              <Link key={index} href={href} className="min-w-[5.8125rem] h-6  ">
+              <Link key={index} href={href} className="lg:min-w-[5.8125rem] h-6  ">
                 <p className="text-zinc-400 text-base font-normal font-['Inter'] leading-normal">{text}</p>
               </Link>
             )
           })}
         </section>
       </header>
-      <main className='w-full sm:w-[1024px] mt-8 px-8' >{children}</main>
+      <main className='w-full lg:w-[1024px] mt-8 px-8' >{children}</main>
     </div>
   )
 }
