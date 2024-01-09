926c9e76567ac46a451a416c715b9b49be3f33a5
Matheus <matheusmendesreis@outlook.com>
2024-01-09 14:11:00 -0300

    feat:remove env

--- matheusmendes.com/src/env.ts
+++ matheusmendes.com/src/env.ts
@@ -2,8 +2,6 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NEXT_PUBLIC_BASE_URL_GITHUB: z.string().url(),
-  NOTION_KEY: z.string(),
-  NOTION_DATABASE_ID: z.string(),
 })
 
 const parsEnv = envSchema.safeParse(process.env)
